5e2d26d6ee612b5a508303d2fdce4dcc9e0d0ae7
박지완 <sat981023@gmail.com>
2025-10-16 13:21:07 +0900

    github action yml commit

--- src/main/java/com/nhnacademy/byeol23front/TestController.java
+++ src/main/java/com/nhnacademy/byeol23front/TestController.java
@@ -1,4 +1,13 @@
 package com.nhnacademy.byeol23front;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+    @GetMapping
+    public String mainPage(){
+
+        return "testPage";
+    }
 }
--- src/test/java/com/nhnacademy/byeol23front/Byeol23FrontApplicationTests.java
+++ src/test/java/com/nhnacademy/byeol23front/Byeol23FrontApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Byeol23FrontApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
