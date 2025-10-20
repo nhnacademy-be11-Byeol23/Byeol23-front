a2429844aaf963d7ab52fbd2f3c2c2cdd39f23c3
박지완 <sat981023@gmail.com>
2025-10-20 15:53:21 +0900

    workflows yml  쉘l 쉘ì쉘ìl의 execute shell script 매개변수 추가

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Byeol23-front
+
--- src/main/java/com/nhnacademy/byeol23front/TestController.java
+++ src/main/java/com/nhnacademy/byeol23front/TestController.java
@@ -1,13 +1,15 @@
 package com.nhnacademy.byeol23front;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@Slf4j
 public class TestController {
     @GetMapping
     public String mainPage(){
-
+        log.info("front");
         return "testPage";
     }
 }
