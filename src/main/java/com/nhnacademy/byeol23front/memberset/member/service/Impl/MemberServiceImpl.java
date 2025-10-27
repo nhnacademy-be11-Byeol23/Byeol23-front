163306878e809d6c57a7c6c3426412ed8594cd5f
Hyeoninii <hyeoninii0314@gmail.com>
2025-10-27 17:23:13 +0900

    feat:로그인, 회원가입 기능 구현

---
+++ src/main/java/com/nhnacademy/byeol23front/memberset/member/service/Impl/MemberServiceImpl.java
@@ -0,0 +1,44 @@
+package com.nhnacademy.byeol23front.memberset.member.service.Impl;
+
+import java.lang.reflect.Member;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.nhnacademy.byeol23front.memberset.member.client.MemberApiClient;
+import com.nhnacademy.byeol23front.memberset.member.dto.LoginRequest;
+import com.nhnacademy.byeol23front.memberset.member.dto.LoginResponse;
+import com.nhnacademy.byeol23front.memberset.member.dto.MemberRegisterRequest;
+import com.nhnacademy.byeol23front.memberset.member.dto.MemberRegisterResponse;
+import com.nhnacademy.byeol23front.memberset.member.service.MemberService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class MemberServiceImpl implements MemberService {
+
+	private final MemberApiClient memberApiClient;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public MemberRegisterResponse register(MemberRegisterRequest request) {
+		request.setPassword(passwordEncoder.encode(request.getPassword()));
+		MemberRegisterResponse response = memberApiClient.registerRequest(request).getBody();
+		log.info("register success");
+		return response;
+	}
+
+	@Override
+	public LoginResponse login(LoginRequest request) {
+		request.setPassword(passwordEncoder.encode(request.getPassword()));
+		LoginResponse response = memberApiClient.loginRequest(request).getBody();
+		log.info("login success");
+		return response;
+	}
+
+
+}
