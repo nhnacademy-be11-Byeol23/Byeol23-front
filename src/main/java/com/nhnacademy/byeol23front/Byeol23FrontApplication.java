a250b21036b3fb4f2064e029ed41e4e54ef1eb5f
박지완 <sat981023@gmail.com>
2025-10-31 13:43:47 +0900

    WIP:save

--- src/main/java/com/nhnacademy/byeol23front/Byeol23FrontApplication.java
+++ src/main/java/com/nhnacademy/byeol23front/Byeol23FrontApplication.java
@@ -2,8 +2,10 @@ package com.nhnacademy.byeol23front;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Byeol23FrontApplication {
 
     public static void main(String[] args) {
