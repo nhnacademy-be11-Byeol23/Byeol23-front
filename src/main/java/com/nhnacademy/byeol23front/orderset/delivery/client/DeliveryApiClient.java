f50084df9d492ffa9714b6252020893faaaba8dc
yuhyun choi <dbgus315@gmail.com>
2025-11-01 22:53:00 +0900

    feat: 관리자 페이지 배송비 정책 페이징 처리

--- src/main/java/com/nhnacademy/byeol23front/orderset/delivery/client/DeliveryApiClient.java
+++ src/main/java/com/nhnacademy/byeol23front/orderset/delivery/client/DeliveryApiClient.java
@@ -1,8 +1,8 @@
 package com.nhnacademy.byeol23front.orderset.delivery.client;
 
-import java.util.List;
-
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +15,7 @@ import com.nhnacademy.byeol23front.orderset.delivery.dto.DeliveryPolicyInfoRespo
 public interface DeliveryApiClient {
 
 	@GetMapping("/api/delivery-policies")
-	ResponseEntity<List<DeliveryPolicyInfoResponse>> getDeliveryPolicies();
+	ResponseEntity<Page<DeliveryPolicyInfoResponse>> getDeliveryPolicies(Pageable pageable);
 
 	@PostMapping("/api/delivery-policies")
 	ResponseEntity<DeliveryPolicyCreateResponse> createDeliveryPolicy(DeliveryPolicyCreateRequest deliveryPolicyCreateRequest);
