5f1bcf798306860f08c00c7000ef91a79486c260
박지완 <sat981023@gmail.com>
2025-11-03 22:16:54 +0900

    feature:coupon-policy

--- src/main/java/com/nhnacademy/byeol23front/couponset/couponpolicy/client/CouponPolicyApiClient.java
+++ src/main/java/com/nhnacademy/byeol23front/couponset/couponpolicy/client/CouponPolicyApiClient.java
@@ -1,9 +1,14 @@
 package com.nhnacademy.byeol23front.couponset.couponpolicy.client;
 
 import com.nhnacademy.byeol23front.couponset.couponpolicy.dto.CouponPolicyCreateRequest;
+import com.nhnacademy.byeol23front.couponset.couponpolicy.dto.CouponPolicyInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 //api/coupon-policy/create
 @FeignClient(name = "BYEOL23-GATEWAY", contextId = "couponPolicyApiClient")
@@ -12,4 +17,6 @@ public interface CouponPolicyApiClient {
     @PostMapping("/api/coupon-policy/create")
     void couponPolicyCreate(@RequestBody CouponPolicyCreateRequest couponPolicyCreateRequest);
 
+    @GetMapping("/api/coupon-policy")
+    ResponseEntity<List<CouponPolicyInfoResponse>> getCouponPolicies();
 }
--- src/main/java/com/nhnacademy/byeol23front/couponset/couponpolicy/controller/CouponPolicyController.java
+++ src/main/java/com/nhnacademy/byeol23front/couponset/couponpolicy/controller/CouponPolicyController.java
@@ -2,12 +2,15 @@ package com.nhnacademy.byeol23front.couponset.couponpolicy.controller;
 
 import com.nhnacademy.byeol23front.couponset.couponpolicy.client.CouponPolicyApiClient;
 import com.nhnacademy.byeol23front.couponset.couponpolicy.dto.CouponPolicyCreateRequest;
+import com.nhnacademy.byeol23front.couponset.couponpolicy.dto.CouponPolicyInfoResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,18 +20,19 @@ public class CouponPolicyController {
     //ApiClient
     private final CouponPolicyApiClient couponPolicyApiClient;
 
-    @GetMapping("/create")
+    @GetMapping
     public String categoryPage(Model model) {
+        ResponseEntity<List<CouponPolicyInfoResponse>> response = couponPolicyApiClient.getCouponPolicies();
         model.addAttribute("pageTitle", "쿠폰 정책 생성");
-
+        model.addAttribute("policies", response.getBody());
         return "admin/coupon/coupon_policy";
     }
 
-    @PostMapping("/create")
+    @PostMapping
     public String createCouponPolicy(CouponPolicyCreateRequest couponPolicyCreateRequest) {
         log.info(couponPolicyCreateRequest.toString());
         couponPolicyApiClient.couponPolicyCreate(couponPolicyCreateRequest);
 
-        return "redirect:/admin/coupon-policy/create";
+        return "redirect:/admin/coupon-policy";
     }
 }
