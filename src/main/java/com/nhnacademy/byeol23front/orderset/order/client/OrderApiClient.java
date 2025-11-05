ca8f7093925fc2e388232e9bcce918ee32dca70e
yuhyun choi <dbgus315@gmail.com>
2025-11-05 22:57:44 +0900

    feat: 주문 상세 관련 Dto 컬럼 추가

--- src/main/java/com/nhnacademy/byeol23front/orderset/order/client/OrderApiClient.java
+++ src/main/java/com/nhnacademy/byeol23front/orderset/order/client/OrderApiClient.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
