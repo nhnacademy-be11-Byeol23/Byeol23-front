1ea1ccc5c253fc0aa59b2014cfdc87bdaf72fb52
yuhyun choi <dbgus315@gmail.com>
2025-11-12 13:52:23 +0900

    modify: 환불 정책 로직 삭제

--- src/main/java/com/nhnacademy/byeol23front/orderset/refundpolicy/dto/RefundPolicyInfoResponse.java
+++
@@ -1,9 +0,0 @@
-package com.nhnacademy.byeol23front.orderset.refundpolicy.dto;
-
-import java.time.LocalDateTime;
-
-public record RefundPolicyInfoResponse(String refundPolicyName,
-									   String refundCondition,
-									   String comment,
-									   LocalDateTime changedAt) {
-}
