ef2ccd265b40a1ca6bcd9e156247b59252d72353
yuhyun choi <dbgus315@gmail.com>
2025-11-13 21:38:21 +0900

    feat:회원 바로 구매 구현

--- src/main/java/com/nhnacademy/byeol23front/bookset/book/dto/BookOrderRequest.java
+++ src/main/java/com/nhnacademy/byeol23front/bookset/book/dto/BookOrderRequest.java
@@ -2,6 +2,5 @@ package com.nhnacademy.byeol23front.bookset.book.dto;
 
 import java.util.List;
 
-public record BookOrderRequest(Long memberId,
-							   List<BookInfoRequest> bookList) {
+public record BookOrderRequest(List<BookInfoRequest> bookList) {
 }
