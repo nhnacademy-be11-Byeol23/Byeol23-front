e0ea1e35b62dee54dfc21b8104066a51ebfe920c
Hyeoninii <hyeoninii0314@gmail.com>
2025-11-13 18:01:07 +0900

    modify:jwt파싱 기능 삭제

--- src/main/java/com/nhnacademy/byeol23front/memberset/exception/DecodeingFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.byeol23front.memberset.exception;
-
-public class DecodeingFailureException extends RuntimeException {
-	public DecodeingFailureException(String message) {
-		super(message);
-	}
-}
--- src/main/java/com/nhnacademy/byeol23front/memberset/exception/keySpecException.java
+++
@@ -1,7 +0,0 @@
-package com.nhnacademy.byeol23front.memberset.exception;
-
-public class keySpecException extends RuntimeException {
-	public keySpecException(String message) {
-		super(message);
-	}
-}
