1690a309e1ef5bd55df00ac9c0e49c8a6d43f53f
Waa-i <ramureezuns@gmail.com>
2025-11-14 14:43:49 +0900

    modify: 카테고리 트리 캐싱 특정 url에서만 호출 되도록 변경

---
+++ src/main/java/com/nhnacademy/byeol23front/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.nhnacademy.byeol23front.config;
+
+import com.nhnacademy.byeol23front.Interceptor.CategoryHeaderInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CategoryHeaderInterceptor())
+                .addPathPatterns("/")
+                .addPathPatterns("/admin/**")
+                .addPathPatterns("/members/login")
+                .addPathPatterns("/mypage/**")
+                .addPathPatterns("/wishlist");
+    }
+}
