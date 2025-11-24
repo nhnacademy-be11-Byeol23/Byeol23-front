4dd9fc761c63dd0c4681f4fd0ca7301937881916
Jinsu Kim <wlstn3697@naver.com>
2025-11-24 11:33:34 +0900

    fix: review

--- src/main/java/com/nhnacademy/byeol23front/reviewset/dto/ReviewResponse.java
+++ src/main/java/com/nhnacademy/byeol23front/reviewset/dto/ReviewResponse.java
@@ -3,6 +3,8 @@ package com.nhnacademy.byeol23front.reviewset.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.nhnacademy.byeol23front.minio.dto.back.GetUrlResponse;
+
 import lombok.Builder;
 
 @Builder
@@ -14,4 +16,20 @@ public record ReviewResponse(
 	LocalDateTime revisedAt,
 	List<String> reviewImageUrls
 ) {
+	public ReviewResponse(
+		Long reviewId,
+		String reviewerName,
+		Byte reviewRate,
+		String reviewContent,
+		LocalDateTime revisedAt,
+		List<String> reviewImageUrls
+	) {
+		this.reviewId = reviewId;
+		this.reviewerName = reviewerName;
+		this.reviewRate = reviewRate;
+		this.reviewContent = reviewContent;
+		this.revisedAt = revisedAt;
+		this.reviewImageUrls = reviewImageUrls.stream()
+			.map(url -> url.replace("http://storage.java21.net:8000/", "https://byeol23.shop/img-proxy/")).toList();
+	}
 }
