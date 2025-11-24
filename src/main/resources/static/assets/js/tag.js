cd5090b0ad96ef8bbe32ea52fa362dbaed0e8718
hyungwooro <hyungwoo.ro@gmail.com>
2025-11-24 16:31:50 +0900

    feat: tag postmapping으로 변경

--- src/main/resources/static/assets/js/tag.js
+++ src/main/resources/static/assets/js/tag.js
@@ -33,8 +33,8 @@ async function updateTag(tagId){
     if (newName === "") {alert("수정할 이름을 입력하십시오"); return;}
     console.log(newName);
     try {
-        const res = await fetch(`/admin/tags/` + tagId, {
-            method: "PUT",
+        const res = await fetch(`/admin/tags/put/` + tagId, {
+            method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify({ tagName: newName })
         });
@@ -57,7 +57,7 @@ function deleteTag(button) {
     const id = button.dataset.id;
     if (!id) return;
 
-    fetch(`/admin/tags/${id}`, { method: "DELETE"})
+    fetch(`/admin/tags/delete/${id}`, { method: "POST"})
         .then((res) => {
             // if (!res.ok) {
             //     return res.json().then(errorBody => { throw new Error(errorBody.message || "삭제 실패"); });
