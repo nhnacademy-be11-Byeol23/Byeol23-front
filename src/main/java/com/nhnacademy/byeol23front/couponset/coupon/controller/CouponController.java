0d340daca8903ac55a6d251e1918a4c1af1e76e2
박지완 <sat981023@gmail.com>
2025-11-24 16:32:13 +0900

    feat:쿠폰발급(카테고리, 도서)

--- src/main/java/com/nhnacademy/byeol23front/couponset/coupon/controller/CouponController.java
+++ src/main/java/com/nhnacademy/byeol23front/couponset/coupon/controller/CouponController.java
@@ -5,6 +5,7 @@ import com.nhnacademy.byeol23front.couponset.coupon.dto.CouponIssueRequestDto;
 import com.nhnacademy.byeol23front.couponset.couponpolicy.client.CouponPolicyApiClient;
 import com.nhnacademy.byeol23front.couponset.couponpolicy.dto.CouponPolicyInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -13,13 +14,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin/coupon")
 @RequiredArgsConstructor
+@Slf4j
 public class CouponController {
     private final CouponPolicyApiClient couponPolicyApiClient;
     private final CouponApiClient couponApiClient;
@@ -35,9 +36,9 @@ public class CouponController {
 
     //쿠폰 발급
     @PostMapping()
-    public ResponseEntity<String> issueCoupon(CouponIssueRequestDto request){
+    public ResponseEntity<String> issueCoupon(@RequestBody CouponIssueRequestDto request){
         String message = couponApiClient.issueCoupon(request).getBody();
-
+        log.info(request.toString());
         return ResponseEntity.ok(message);
     }
 }
--- src/main/java/com/nhnacademy/byeol23front/couponset/coupon/dto/CouponIssueRequestDto.java
+++ src/main/java/com/nhnacademy/byeol23front/couponset/coupon/dto/CouponIssueRequestDto.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.byeol23front.couponset.coupon.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 
 public record CouponIssueRequestDto(
