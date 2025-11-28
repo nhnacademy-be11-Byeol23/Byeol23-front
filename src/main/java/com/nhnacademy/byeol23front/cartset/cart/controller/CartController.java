6b1a798b44de5c7fa98d050a5046d158befe3e0a
Waa-i <ramureezuns@gmail.com>
2025-11-28 19:32:49 +0900

    modify: 장바구니 뷰 수정

--- src/main/java/com/nhnacademy/byeol23front/cartset/cart/controller/CartController.java
+++ src/main/java/com/nhnacademy/byeol23front/cartset/cart/controller/CartController.java
@@ -4,23 +4,55 @@ import com.nhnacademy.byeol23front.cartset.cart.client.CartApiClient;
 import com.nhnacademy.byeol23front.cartset.cart.dto.CartBookAddRequest;
 import com.nhnacademy.byeol23front.cartset.cart.dto.CartBookResponse;
 import com.nhnacademy.byeol23front.cartset.cart.dto.CartBookUpdateRequest;
+import com.nhnacademy.byeol23front.cartset.cart.dto.FreeShippingCondition;
+import com.nhnacademy.byeol23front.orderset.delivery.client.DeliveryApiClient;
+import com.nhnacademy.byeol23front.orderset.delivery.dto.DeliveryPolicyInfoResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.StructuredTaskScope;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class CartController {
     private final CartApiClient cartApiClient;
+    private final DeliveryApiClient deliveryApiClient;
 
     @GetMapping("/carts/books")
-    public String cartPage(Model model) {
-        List<CartBookResponse> cartBooks = cartApiClient.getCartBooks();
-        model.addAttribute("cartBooks", cartBooks);
-        return "cart/carts";
+    public String cartPage(Model model) throws InterruptedException, ExecutionException {
+        try(var taskScope = new StructuredTaskScope.ShutdownOnFailure()) {
+            ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+            var cartTask = taskScope.fork(() -> {
+                RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(attributes.getRequest()));
+                return cartApiClient.getCartBooks();
+            });
+            var deliveryTask = taskScope.fork(() -> {
+                RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(attributes.getRequest()));
+                return deliveryApiClient.getCurrentDeliveryPolicy();
+            });
+
+            taskScope.join();
+            taskScope.throwIfFailed();
+
+            List<CartBookResponse> cartBooks = cartTask.get();
+            ResponseEntity<DeliveryPolicyInfoResponse> currentDeliveryPolicy = deliveryTask.get();
+            DeliveryPolicyInfoResponse deliveryPolicy = currentDeliveryPolicy.getBody();
+            FreeShippingCondition freeShippingCondition = new FreeShippingCondition(deliveryPolicy.freeDeliveryCondition().intValue(), deliveryPolicy.deliveryFee().intValue());
+
+            model.addAttribute("cartBooks", cartBooks);
+            model.addAttribute("shippingCondition", freeShippingCondition);
+            return "cart/carts";
+        }
     }
 
     @PostMapping("/carts/books")
