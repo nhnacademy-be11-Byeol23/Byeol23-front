ed063953e413eeb11b14e7e513e658da53f4a223
Jinsu Kim <wlstn3697@naver.com>
2025-11-29 13:19:27 +0900

    fix: point policy

--- src/main/java/com/nhnacademy/byeol23front/point/controller/PointAdminViewController.java
+++ src/main/java/com/nhnacademy/byeol23front/point/controller/PointAdminViewController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+import com.nhnacademy.byeol23front.point.dto.ReservedPolicy; // 추가
+
 @Controller
 @RequestMapping("/admin/point-policy")
 @RequiredArgsConstructor
@@ -15,6 +17,8 @@ public class PointAdminViewController {
 	@GetMapping
 	public String pointPolicyPage(Model model) {
 		model.addAttribute("pageTitle", "포인트 정책 관리");
+		// ReservedPolicy 값을 뷰에서 선택할 수 있도록 추가
+		model.addAttribute("reservedPolicies", ReservedPolicy.values());
 		return "admin/point/point_policy";
 	}
 }
