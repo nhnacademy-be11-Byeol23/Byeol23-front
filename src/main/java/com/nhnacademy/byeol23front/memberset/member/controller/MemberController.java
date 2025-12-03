7d14613786323661249faa192ea633030287beff
Hyeoninii <hyeoninii0314@gmail.com>
2025-12-03 13:04:11 +0900

    fix:loginRequestTmp

--- src/main/java/com/nhnacademy/byeol23front/memberset/member/controller/MemberController.java
+++ src/main/java/com/nhnacademy/byeol23front/memberset/member/controller/MemberController.java
@@ -16,11 +16,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -28,7 +26,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.nhnacademy.byeol23front.memberset.member.service.MemberService;
 
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
--- src/main/java/com/nhnacademy/byeol23front/memberset/member/dto/LoginRequestTmp.java
+++ src/main/java/com/nhnacademy/byeol23front/memberset/member/dto/LoginRequestTmp.java
@@ -2,15 +2,10 @@ package com.nhnacademy.byeol23front.memberset.member.dto;
 
 import java.util.List;
 
-import jakarta.validation.constraints.Null;
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class LoginRequestTmp {
-	String loginId;
-	String loginPassword;
-	String redirectUrl;
-	private List<Long> bookIds;
-	private List<Integer> quantities;
-}
+public record LoginRequestTmp(
+	String loginId,
+	String loginPassword,
+	String redirectUrl,
+	List<Long> bookIds,
+	List<Integer> quantities
+) {}
