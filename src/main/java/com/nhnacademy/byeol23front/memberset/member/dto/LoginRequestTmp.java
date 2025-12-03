54ae81ad32e6811d72aebdffccd9179b42306162
박지완 <sat981023@gmail.com>
2025-12-03 23:59:26 +0900

    modify:LoginRequestTmp class로 변경

--- src/main/java/com/nhnacademy/byeol23front/memberset/member/dto/LoginRequestTmp.java
+++ src/main/java/com/nhnacademy/byeol23front/memberset/member/dto/LoginRequestTmp.java
@@ -1,11 +1,16 @@
 package com.nhnacademy.byeol23front.memberset.member.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.List;
 
-public record LoginRequestTmp(
-	String loginId,
-	String loginPassword,
-	String redirectUrl,
-	List<Long> bookIds,
-	List<Integer> quantities
-) {}
+@Getter
+@Setter
+public class LoginRequestTmp {
+    String loginId;
+    String loginPassword;
+    String redirectUrl;
+    List<Long> bookIds;
+    List<Integer> quantities;
+}
