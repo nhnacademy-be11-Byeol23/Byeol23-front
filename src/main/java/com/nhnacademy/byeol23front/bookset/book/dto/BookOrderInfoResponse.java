7093d42cc7dc7c1ba631d16b93b0bd95d77a9166
박지완 <sat981023@gmail.com>
2025-12-03 23:16:08 +0900

    WIP:revert 취소

--- src/main/java/com/nhnacademy/byeol23front/bookset/book/dto/BookOrderInfoResponse.java
+++ src/main/java/com/nhnacademy/byeol23front/bookset/book/dto/BookOrderInfoResponse.java
@@ -5,9 +5,9 @@ import java.math.BigDecimal;
 import com.nhnacademy.byeol23front.orderset.packaging.dto.PackagingInfoResponse;
 
 public record  BookOrderInfoResponse(Long bookId,
-									 String firstImageUrl,
-									 String bookTitle,
-									 int quantity,
-									 BigDecimal price,
-									 PackagingInfoResponse packaging) {
+                                     String firstImageUrl,
+                                     String bookTitle,
+                                     int quantity,
+                                     BigDecimal price,
+                                     PackagingInfoResponse packaging) {
 }
